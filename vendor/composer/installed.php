7f2532b0d372134d15e10d538f37ca1c39c146e6
sdonkers <s.donkers@sivard.nl>
2026-04-28 10:57:46 +0000

    Build from release-v1.10.2-fork

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/search-replace-for-block-editor',
+        'pretty_version' => 'dev-release-v1.10.2-fork-built',
+        'version' => 'dev-release-v1.10.2-fork-built',
+        'reference' => '02bc84b4d5ae1c14292d29e68b48136fe1dd46e2',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/search-replace-for-block-editor' => array(
+            'pretty_version' => 'dev-release-v1.10.2-fork-built',
+            'version' => 'dev-release-v1.10.2-fork-built',
+            'reference' => '02bc84b4d5ae1c14292d29e68b48136fe1dd46e2',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
