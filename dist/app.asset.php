8a538807d942195037ce86215ed325d4ed11b7e1
sdonkers <s.donkers@sivard.nl>
2026-04-28 11:31:34 +0000

    Build from release-v1.10.1-fork2

---
+++ dist/app.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keyboard-shortcuts', 'wp-primitives'), 'version' => '8d7725892a5d1cca0e77');
