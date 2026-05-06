a5363987a1662e5f9590bbca35c4457e8fe55991
sdonkers <s.donkers@sivard.nl>
2026-05-06 14:57:28 +0000

    Build from release-1.10.1-nrc

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/search-replace-for-block-editor',
+        'pretty_version' => 'dev-release-1.10.1-nrc-built',
+        'version' => 'dev-release-1.10.1-nrc-built',
+        'reference' => 'a8dc26ccceb76613b3dded32c0ea9ce6b8a4531d',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/search-replace-for-block-editor' => array(
+            'pretty_version' => 'dev-release-1.10.1-nrc-built',
+            'version' => 'dev-release-1.10.1-nrc-built',
+            'reference' => 'a8dc26ccceb76613b3dded32c0ea9ce6b8a4531d',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
