a3319c155b0df948a85d90fcae681faf97d31e92
sdonkers <s.donkers@sivard.nl>
2026-05-06 15:07:29 +0000

    push to master-built

--- bin/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wp-env run cli wp theme activate twentytwentythree
-wp-env run cli wp rewrite structure /%postname%
-wp-env run cli wp option update blogname "Search & Replace for Block Editor"
-wp-env run cli wp option update blogdescription "Search and Replace text within the Block Editor."
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/search-replace-for-block-editor',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '13da662040c15e879f274549fea1afa734e97bd7',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/search-replace-for-block-editor' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '13da662040c15e879f274549fea1afa734e97bd7',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
